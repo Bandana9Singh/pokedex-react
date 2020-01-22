bad5340d5d07cd03f38988a45d62cf8d26b84eed
Bandana9Singh <bandana.singh@bloomreach.com>
2020-01-22 14:49:46 +0530

    Initial commit

---
+++ src/Pokeframe/pokeframe.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+//This component will make call to PokeApi
+export default class Pokeframe extends React.Component {
+    //Store states here, constructor get called first when the component is created
+    constructor() {
+        super();
+        this.state = { 
+            pokemons: []
+        }
+    }
+    //Called after DOM load. Initialisation that require DOM nodes, can go here.
+    componentDidMount() {
+        fetch("https://pokeapi.co/api/v2/pokemon/")
+        .then(res => res.json())
+        .then(response => {
+            this.setState({pokemons: response.results});
+            console.log(this.state);
+        })
+    }
+    //Call the methods with ES6 annotation, render is mandatory for components, others are optional
+    render = () => {
+        return (
+            //TODO: Create a new component for each Pokemon card.
+            <div>
+                <div>Render Poke-Frame</div>
+                <div>
+                    <ul>
+                        { this.state.pokemons.map((pokemon,index) =>
+                            <li key={index}>
+                                {pokemon.name}
+                            </li>    
+                        )}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
